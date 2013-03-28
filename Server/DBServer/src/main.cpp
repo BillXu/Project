2b56e3150d9ff9edb567118540bb5b65b32b803b
WenyanXu <xuwenyanbill@gmail.com>
2013-03-28 22:28:37 +0800

    test thread pass

--- Server/DBServer/src/main.cpp
+++ Server/DBServer/src/main.cpp
@@ -1,7 +1,20 @@
 #include"main.h"
 #include <iostream>
+#include "ThreadMod.h"
+class A
+	:public CThreadT
+{
+public:
+	void __run()
+	{
+		printf("this function \n") ;
+	}
+};
+
 int main()
 {
+	A a ;
+	a.Start() ;
 	getchar();
-	return 0 ;
+	return 0 ; 
 }
