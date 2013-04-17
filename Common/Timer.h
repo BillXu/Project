4a12166822ccc088ad91bb1981de5dc14372814a
WenyanXu <xuwenyanbill@gmail.com>
2013-04-18 01:03:59 +0800

    add timer

---
+++ Common/Timer.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <map>
+#include <vector>
+class CTimer ;
+class CTimerDelegate
+{
+public:
+	typedef void (CTimerDelegate::*lpTimerSelector)(float fTimeElaps );
+public:
+	CTimerDelegate();
+	virtual ~CTimerDelegate();
+	virtual void Update(float fTimeElpas );
+	void SetEnableUpdate( bool bEnable );
+protected:
+	CTimer* m_pUpdateTimer ;
+};
+
+class CTimer
+{
+public:
+	enum eTimerState
+	{
+		eTimerState_None,
+		eTimerState_Stop,
+		eTimerState_Pause,
+		eTimerState_Runing,
+		//eTimerState_Delaying,
+		eTimerState_Max,
+	};
+public:
+	CTimer(CTimerDelegate* pDeleate,CTimerDelegate::lpTimerSelector pFunc , float fInterval );
+	~CTimer();
+	void Pause(){ m_eState = eTimerState_Pause ;}
+	void Resume(){ m_eState = eTimerState_Runing ;}
+	void Reset();
+	void Update( float fTimeElaps );
+	void SetDelayTime( float fTimerDelay ){ m_fDelay = fTimerDelay ;}
+	void SetInterval(float fNewInterval ){ m_fInterval = fNewInterval ;}
+	void Stop(){ m_eState = eTimerState_Stop ;}
+	bool IsStop(){ return m_eState == eTimerState_Stop ;} 
+	bool IsRuning(){ return eTimerState_Runing == m_eState ; }
+	unsigned int GetTimerID(){ return m_nTimerID ;}
+	void Start(){m_eState = eTimerState_Runing ;}
+protected:
+	CTimerDelegate* m_pDelegate ;
+	CTimerDelegate::lpTimerSelector m_pTimerFunc ;
+
+	float m_fDelay ;
+	float m_fInterval ;
+
+	eTimerState m_eState ;
+ 
+	float m_fDelayKeeper ;
+	float m_fIntervalKeeper ;
+
+	static unsigned int s_TimerCount ;
+	unsigned int m_nTimerID ;
+};
+
+class CTimerManager
+{
+public:
+	typedef std::map<unsigned int ,CTimer*> MAP_TIMERS ;
+	typedef std::vector<unsigned int> VEC_TIMER_WILL_REMOVE ;
+public:
+	static CTimerManager* SharedTimerManager();
+	CTimerManager();
+	~CTimerManager();
+	CTimer* AddTimer(CTimer* pTimer );
+	CTimer* AddTimer( CTimerDelegate* pDelegate, CTimerDelegate::lpTimerSelector pFunc );
+	void Update();
+	void SetTimeScale( float fScale ){ m_fTimerScale = fScale ;}
+	float GetTimeScale(){ return m_fTimerScale ;} 
+	void RemoveTimer(CTimer* pTimer ){ if ( pTimer)RemoveTimer(pTimer->GetTimerID());}
+	void RemoveTimer( unsigned int nTimerID ); 
+	CTimer* GetTimer(unsigned int nTimerID );
+protected:
+	MAP_TIMERS m_vAllTimers ;
+	VEC_TIMER_WILL_REMOVE m_vTimerWillRemove;
+	float m_fTimerScale ;
+};
