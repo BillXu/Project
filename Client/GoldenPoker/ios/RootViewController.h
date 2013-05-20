df020a61f4f9433951f769b5953861c45583739e
BillXu <xuwenyanbill@gmail.com>
2013-05-20 15:32:52 +0800

    add client

---
+++ Client/GoldenPoker/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  GoldenPokerAppController.h
+//  GoldenPoker
+//
+//  Created by Xu BILL on 13-5-20.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
